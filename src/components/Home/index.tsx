2e73fc2a5fdaa2bf3937b94ec8151f01bd85d08f
jaroncui <jaron.cui@gmail.com>
2024-04-03 19:57:42 -0400

    created start game panel and reorganized home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,37 @@
 import Nav from "../../Nav";
 import { Link } from "react-router-dom";
 import GameList from "./GameList";
+import { Button, Col, Container, Row, Stack } from "react-bootstrap";
+
+function StartGamePanel() {
+  return (
+    <div style={{borderStyle: 'solid', padding: '10px'}}>
+      <h3>Play</h3>
+      <Stack gap={2}>
+        <Button>Random Match</Button>
+        <Button>Play with Friend</Button>
+      </Stack>
+    </div>
+  );
+}
 
 function Home() {
   return (
     <div>
       <Nav />
-      <h1>Home</h1>
-      <Link to="/Game">
-        <button>Create New Game</button>
-      </Link>
-      <GameList />
+      <div>
+        <Container style={{marginLeft: '5%', marginRight: '5%'}}>
+          <h1>Home</h1>
+          <Row>
+            <Col className="col-8">
+              <StartGamePanel/>
+            </Col>
+            <Col>
+              <GameList />
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
