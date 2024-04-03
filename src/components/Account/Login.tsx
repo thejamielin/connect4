940f10c9395793568a201d26afc2d8bb8552ba44
Jamie Lin <jamie.miki.lin@gmail.com>
2024-04-03 19:57:13 -0400

    reformat with prettier, revert back to this commit after trying reducer logic, could not because reducer does not support promises

--- src/components/Account/Login.tsx
+++ src/components/Account/Login.tsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 import Nav from "../../Nav";
 import { useNavigate } from "react-router";
 import axios from "axios";
-import { apiAccountCheckSession, apiAccountLogin, cacheSessionToken, deleteSessionToken, getSessionToken } from "../../dao";
+import {
+  apiAccountCheckSession,
+  apiAccountLogin,
+  cacheSessionToken,
+  deleteSessionToken,
+  getSessionToken,
+} from "../../dao";
 import Inputs from "./Inputs";
 
 function Login() {
   const navigate = useNavigate();
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [loggedIn, setLoggedIn] = useState<boolean>();
 
   useEffect(() => {
@@ -17,7 +23,7 @@ function Login() {
       setLoggedIn(false);
       return;
     }
-    apiAccountCheckSession(token).then(isValidSession => {
+    apiAccountCheckSession(token).then((isValidSession) => {
       setLoggedIn(isValidSession);
       isValidSession || deleteSessionToken();
     });
@@ -25,47 +31,49 @@ function Login() {
 
   useEffect(() => {
     if (loggedIn === true) {
-      navigate('/home');
+      navigate("/home");
     }
   }, [loggedIn]);
 
   if (loggedIn === undefined) {
-    return <div>Loading</div>
+    return <div>Loading</div>;
   }
 
   const FIELDS = [
     {
-      name: 'username',
+      name: "username",
       set: setUsername,
       value: username,
     },
     {
-      name: 'password',
+      name: "password",
       set: setPassword,
       value: password,
-      hide: true
-    }
-  ]
+      hide: true,
+    },
+  ];
   function login() {
     if (!username || !password) {
       return;
     }
-    apiAccountLogin(username, password).then(token => {
-      cacheSessionToken(token);
-      navigate('/home');
-    }).catch(() => {
-      // TODO: handle login fail
-    });
+    apiAccountLogin(username, password)
+      .then((token) => {
+        cacheSessionToken(token);
+        navigate("/home");
+      })
+      .catch(() => {
+        // TODO: handle login fail
+      });
   }
 
   return (
     <div>
       <Nav />
       <h1>Login</h1>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <Inputs fields={FIELDS}/>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <Inputs fields={FIELDS} />
         <button onClick={login}>Log in</button>
-        <button onClick={() => navigate('/register')}>Go Register</button>
+        <button onClick={() => navigate("/register")}>Go Register</button>
       </div>
     </div>
   );
