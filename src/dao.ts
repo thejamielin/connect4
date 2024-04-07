09dee3fd49a8dca1f083f83b54d0190ab0af3220
Jamie Lin <jamie.miki.lin@gmail.com>
2024-04-07 13:12:18 -0400

    format using prettier

--- src/dao.ts
+++ src/dao.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { GameResult } from "./types";
 
-const COOKIE_TOKEN_NAME = 'token';
+const COOKIE_TOKEN_NAME = "token";
 export function cacheSessionToken(token: string) {
   localStorage.setItem(COOKIE_TOKEN_NAME, token);
 }
@@ -14,16 +14,16 @@ export function getSessionToken(): string | undefined {
   return localStorage.getItem(COOKIE_TOKEN_NAME) || undefined;
 }
 
-export function validateLoggedIn(setLoggedIn : (isLoggedIn: boolean) => void) {
+export function validateLoggedIn(setLoggedIn: (isLoggedIn: boolean) => void) {
   const token = getSessionToken();
-    if (!token) {
-      setLoggedIn(false);
-      return;
-    }
-    apiAccountCheckSession(token).then((isValidSession) => {
-      setLoggedIn(isValidSession);
-      isValidSession || deleteSessionToken();
-    });
+  if (!token) {
+    setLoggedIn(false);
+    return;
+  }
+  apiAccountCheckSession(token).then((isValidSession) => {
+    setLoggedIn(isValidSession);
+    isValidSession || deleteSessionToken();
+  });
 }
 
 const API_BASE = process.env.REACT_APP_API_BASE;
@@ -38,34 +38,50 @@ const USER = `${API_BASE}/user`;
 
 export interface GameSearchParameters {
   count: number;
-  sort?: 'newest' | 'oldest';
+  sort?: "newest" | "oldest";
   filter?: {
     players?: string[];
-  }
+  };
 }
 
-export async function apiGamesSearch(searchParams: GameSearchParameters): Promise<GameResult[]> {
+export async function apiGamesSearch(
+  searchParams: GameSearchParameters
+): Promise<GameResult[]> {
   const response = await axios.post(GAMES_SEARCH, searchParams);
   return response.data;
 }
 
-export async function apiAccountLogin(username: string, password: string): Promise<string> {
-  const response = await axios.post(ACCOUNT_LOGIN, {username: username, password: password});
+export async function apiAccountLogin(
+  username: string,
+  password: string
+): Promise<string> {
+  const response = await axios.post(ACCOUNT_LOGIN, {
+    username: username,
+    password: password,
+  });
   return response.data.token;
 }
 
-export async function apiAccountRegister(username: string, password: string, email: string): Promise<string> {
-  const response = await axios.post(ACCOUNT_REGISTER, {username: username, password: password, email: email});
+export async function apiAccountRegister(
+  username: string,
+  password: string,
+  email: string
+): Promise<string> {
+  const response = await axios.post(ACCOUNT_REGISTER, {
+    username: username,
+    password: password,
+    email: email,
+  });
   return response.data.token;
 }
 
 export async function apiAccountCheckSession(token: string) {
-  const response = await axios.post(ACCOUNT_CHECKSESSION, {token: token});
+  const response = await axios.post(ACCOUNT_CHECKSESSION, { token: token });
   return response.data.isValidSession;
 }
 
 export async function apiAccountLogout() {
-  await axios.post(ACCOUNT_LOGOUT, {token: getSessionToken()});
+  await axios.post(ACCOUNT_LOGOUT, { token: getSessionToken() });
 }
 
 export interface PictureInfo {
@@ -79,13 +95,17 @@ export interface PictureInfo {
   likes: string[];
 }
 
-export async function apiPictureSearch(searchString: string): Promise<PictureInfo[]> {
-  const response = await axios.get(PICTURES_SEARCH, { params: { q: searchString } });
+export async function apiPictureSearch(
+  searchString: string
+): Promise<PictureInfo[]> {
+  const response = await axios.get(PICTURES_SEARCH, {
+    params: { q: searchString },
+  });
   return response.data;
 }
 
 export async function apiPictureId(imageID: string): Promise<PictureInfo[]> {
-  const response = await axios.get(PICTURES_ID, { params: { id: imageID } });
+  const response = await axios.get(`${PICTURES_ID}/${imageID}`);
   return response.data;
 }
 
@@ -103,7 +123,11 @@ export interface UserStats {
   // TODO: Add stat fields
 }
 
-export async function apiSetUser(user: Partial<Pick<User, 'email' | 'following' | 'pfp'>>): Promise<boolean> {
-  const response = await axios.put(USER, { body: { token: getSessionToken(), editedFields: user }});
+export async function apiSetUser(
+  user: Partial<Pick<User, "email" | "following" | "pfp">>
+): Promise<boolean> {
+  const response = await axios.put(USER, {
+    body: { token: getSessionToken(), editedFields: user },
+  });
   return response.data.success;
 }
