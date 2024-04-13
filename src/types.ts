effc549b3dab274b4cbc199ec888119993604f83
jaroncui <jaron.cui@gmail.com>
2024-04-13 16:46:55 -0400

    implemented game over screen and game results

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,3 @@
-export interface GameResult {
-  id: string;
-  player1: string;
-  player2: string;
-  // if winner is undefined, the game ended in a draw
-  winner?: string;
-  //moves: Move[];
-  date: Date;
-}
-
 export type User = RegularUser | BeginnerUser;
 
 export interface RegularUser {
