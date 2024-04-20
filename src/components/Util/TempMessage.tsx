20fccb6b9ac5337fb6f1e482f93cb33ddce43eb1
Jamie Lin <jamie.miki.lin@gmail.com>
2024-04-20 18:27:04 -0400

    update TempMessage to include NavBar so loading isn't as jarring

--- src/components/Util/TempMessage.tsx
+++ src/components/Util/TempMessage.tsx
@@ -1,7 +1,15 @@
-export default function TempMessage({text} : {text: string}) {
+import C4Nav from "../../Nav";
+import { useSelector } from "react-redux";
+import { Connect4State } from "../../store";
+
+export default function TempMessage({ text }: { text: string }) {
+  const userData = useSelector(
+    (state: Connect4State) => state.accountReducer.userData
+  );
   return (
-    <div style={{fontSize: "40px", padding: "20px"}}>
-      {text}
+    <div>
+      <C4Nav userData={userData} />
+      <div style={{ fontSize: "40px", padding: "20px" }}>{text}</div>
     </div>
   );
 }
